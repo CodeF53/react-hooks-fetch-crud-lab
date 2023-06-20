06863cb71ed10d66b75df44888f5f7835336e424
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-06-20 12:59:25 -0400

    Revert "code"
    This reverts commit b001837ede8b43ff0d6cc807a607f3b2532f1c51.
    
    reverse

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-<<<<<<< HEAD
   const [questions,setQuestions]=useState([])
 
   const URL="http://localhost:4000/questions"
@@ -31,13 +30,11 @@ function App() {
     .then(res=>res.json())
     .then(newQuestion=>setQuestions([...questions,newQuestion]))
   }
-=======
->>>>>>> parent of b343ffa (code)
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestion={addQuestion} /> : <QuestionList questions={questions} />}
     </main>
   );
 }
