b343ffa8663c765c9b8fb697b41db27526d06142
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-06-20 10:14:53 -0400

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions,setQuestions]=useState([])
+
+  const URL="http://localhost:3000/questions"
+
+  useEffect(()=>{
+    loadQuestions(URL)
+  },[])
+
+  function loadQuestions(URL){
+    fetch(URL)
+    .then(res=>res.json())
+    .then(questions=>setQuestions(questions))
+  }
+
+  function addQuestion(question){
+    fetch(URL,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(question)
+    })
+    .then(res=>res.json())
+    .then(newQuestion=>setQuestions([...questions,newQuestion]))
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestion={addQuestion} /> : <QuestionList questions={questions} />}
     </main>
   );
 }
